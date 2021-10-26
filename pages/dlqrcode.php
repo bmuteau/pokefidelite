685c848313e90028079304ca2201f93cb7c92ec5
Brandon Muteau <muteaub@gmail.com>
2021-10-26 16:01:07 +0200

    download page for qrcode finish, need the qrcode générator

---
+++ pages/dlqrcode.php
@@ -0,0 +1,16 @@
+<?php
+// header
+include 'C:\wamp64\www\pokefidelite\template\header.php ';
+
+?>
+<link href="../styles/dlqrcode.css" rel="stylesheet">
+<h1>Télécharger votre QrCode</h1>
+
+
+<!-- QRCODE Généré et affiché ici -> 1 QrCode par adresse mail  -->
+
+<input type="button" class="dl" value="Télécharger le QrCode">
+<?php
+// footer
+include 'C:\wamp64\www\pokefidelite\template\footer.php';
+?>
