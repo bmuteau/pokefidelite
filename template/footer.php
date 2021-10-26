01af90db27e3926c5f32e01df6bde7c6231ed518
Brandon Muteau <muteaub@gmail.com>
2021-10-26 10:22:43 +0200

    all template

---
+++ template/footer.php
@@ -0,0 +1,9 @@
+<link href="../styles/footer.css" rel="stylesheet">
+
+<div class="footer">
+    <ul>
+        <li> <img src="../src/iconfinder_facebook_social media_social_317727.svg" class="fblogo"></li>
+        <li> <img src="../src/iconfinder_camera_instagram_instagram logo_5296765.svg" class="instalogo"></li>
+        <li><img src="../src/_FR-removebg-preview.png" class="weblogo"></li>
+    </ul>
+</div>
---
+++ template/header.php
@@ -0,0 +1,5 @@
+<link href="../styles/header.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<img src="../src/logo.png" alt="logo_poke" class="logo">
