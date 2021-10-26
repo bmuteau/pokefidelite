3166ae810cb72486c852ff214eba41630b1701eb
Brandon Muteau <muteaub@gmail.com>
2021-10-26 16:02:17 +0200

    home page, need session variable

---
+++ pages/home.php
@@ -0,0 +1,53 @@
+<?php
+// header
+include 'C:\wamp64\www\pokefidelite\template\header.php ';
+
+?>
+<link href="../styles/home.css" rel="stylesheet">
+
+<div class="menu-bg"></div>
+<div class="menu-burger">☰</div>
+<div class="menu-items">
+    <div>• Accueil </div>
+    <div>• Mes informations</div>
+    <div>• Fonctionnement de la carte de fidélité </div>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <div>• Nous contacter</div>
+    <div>• Mention légale </div>
+    <div>• Politique de confidentialité</div>
+</div>
+
+<div class="content">
+    <h2>Prochaine réduction dans : </h2>
+    <p><span> &nbsp;VARIABLE points &nbsp; </span></p>
+    <h2>=</h2>
+    <p class="reduct">10% de réduction</p>
+    <h2>Mes points actuels :</h2>
+    <p><span> &nbsp;VARIABLE2 &nbsp; </span></p>
+
+    <!-- QRCODE -->
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <p><span>&nbsp;Mon QrCode&nbsp;<br />
+            &nbsp; à présenté &nbsp;<br />
+            &nbsp;à chaque passage en&nbsp; </br>
+            &nbsp; caisse&nbsp;</span></p>
+</div>
+
+<script src="../script/navbar.js"></script>
+
+<?php
+// footer
+include 'C:\wamp64\www\pokefidelite\template\footer.php';
+?>
