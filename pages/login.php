a47912e87694c8b259590cfd55e0d9f2f3f50451
Brandon Muteau <muteaub@gmail.com>
2021-10-26 16:02:47 +0200

    login page, need the back end

---
+++ pages/login.php
@@ -0,0 +1,25 @@
+<?php
+// header
+include 'C:\wamp64\www\pokefidelite\template\header.php ';
+
+?>
+<link href="../styles/connexionregister.css" rel="stylesheet">
+<p class="cf">Compte de fidélité</p>
+
+
+<form class="loginplace">
+
+    <label>Email</label>
+    <input type="email" style="font-size: 51px;" value="" autocomplete="off" requiered>
+    <label>Mot de passe</label>
+    <input type="password" style="font-size: 51px;" value="" autocomplete="off" required>
+
+    <input type="submit" class="login" value="Se connecter">
+</form>
+
+
+<?php
+// footer
+include 'C:\wamp64\www\pokefidelite\template\footer.php';
+?>
+<p class="mlu">Mention légales d'utilisation</p>
