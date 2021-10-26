3c2569c144f45ed93258c54709cf6b198cfa1750
Brandon Muteau <muteaub@gmail.com>
2021-10-26 10:20:42 +0200

    home page (need the mlu href)&

---
+++ pages/connexionregister.php
@@ -0,0 +1,20 @@
+<?php
+// header
+include 'C:\wamp64\www\pokefidelite\template\header.php ';
+
+?>
+<link href="../styles/connexionregister.css" rel="stylesheet">
+<p class="cf">Compte de fidélité</p>
+
+<body>
+
+    <button class="signin">S'inscrire</button>
+    <button class="register">Se connecter</button>
+
+</body>
+
+<?php
+// footer
+include 'C:\wamp64\www\pokefidelite\template\footer.php';
+?>
+<p class="mlu">Mention légales d'utilisation</p>
