f347259574e38ce1228414b7f4e64ca94b11dcbe
Brandon Muteau <muteaub@gmail.com>
2021-10-26 10:21:17 +0200

    make the register page

---
+++ pages/register.php
@@ -0,0 +1,31 @@
+<?php
+// header
+include 'C:\wamp64\www\pokefidelite\template\header.php ';
+
+?>
+<link href="../styles/connexionregister.css" rel="stylesheet">
+<p class="cf">Compte de fidélité</p>
+
+
+<form>
+
+    <label>Nom</label>
+    <input type="text">
+    <label>Prénom</label>
+    <input type="text">
+    <label>Email</label>
+    <input type="email">
+    <label>Mot de passe</label>
+    <input type="password">
+    <label>Code postal</label>
+    <input type="number">
+
+
+
+</form>
+
+<?php
+// footer
+include 'C:\wamp64\www\pokefidelite\template\footer.php';
+?>
+<p class="mlu">Mention légales d'utilisation</p>
