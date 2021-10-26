a9122ef9989e8e9615dc624ad4ab23f5c0b8be98
Brandon Muteau <muteaub@gmail.com>
2021-10-26 16:04:56 +0200

    add jquery cdn

--- template/header.php
+++ template/header.php
@@ -1,5 +1,6 @@
 <link href="../styles/header.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" crossorigin="anonymous"></script>
 
 <img src="../src/logo.png" alt="logo_poke" class="logo">
